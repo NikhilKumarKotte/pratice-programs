b0ccfa442b0229493dd41c974bec9f769ef6dd86
asuranslayer <2120.nikhil@gmail.com>
2026-09-01 23:24:08 +0530

    v14

---
+++ utilities/qrcode2.py
@@ -0,0 +1,38 @@
+import qrcode
+
+print("================================")
+print("       📱 QR CODE GENERATOR")
+print("================================")
+
+data = input("Enter text or URL: ")
+
+if data.strip() == "":
+    print("❌ You didn't enter anything.")
+else:
+
+    filename = input(
+        "Enter file name (without .png): "
+    )
+
+    if filename.strip() == "":
+        filename = "qr_code"
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4
+    )
+
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    image = qr.make_image(
+        fill_color="black",
+        back_color="white"
+    )
+
+    image.save(filename + ".png")
+
+    print("\n✅ QR CODE CREATED!")
+    print("📁 File:", filename + ".png")
